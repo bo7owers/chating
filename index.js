208f79b2f7040917130a4f19ea13e335cfa32251
René Torres <7orresrene@users.noreply.github.com>
2021-05-15 22:52:51 -0400

    Started with jquery

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 $(document).ready(function () {
+    let $man = $('.persona1');
+    let $woman = $('.persona2');
+    let $nxtBtn = $('#nextBtn');
+    let $dialogue = $('.dialogue');
+
+    // dialogues 
+    let dialogues = {
+        man: "Man",
+        phrase1: "Hello there"
+    }
     
+    $($nxtBtn).on('click', () => {
+
+    })
+
+
+
+
+
+
 });
